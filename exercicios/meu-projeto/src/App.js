66ab7c5e195a7b293a1bd02100c0905a7ca7b005
Pedro Henrique <92455469+eipedrohenrique@users.noreply.github.com>
2022-04-29 16:48:15 -0300

    aula

--- exercicios/meu-projeto/src/App.js
+++ exercicios/meu-projeto/src/App.js
@@ -1,17 +1,22 @@
-import './App.css';
-import OutraLista from './components/OutraLista';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
-
-  const meusItens = ['React', 'Vue', 'Angular']
-
+  
   return (
-    <div className="App">
-      <h1>Renderização de Listas</h1>
-      <OutraLista itens={meusItens} />
-      <OutraLista itens={[]} />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/empresa" element={<Empresa/>}></Route>
+        <Route path="/contato" element={<Contato/>}></Route>
+      </Routes>
+      <Footer/>
+    </Router>
   )
 }
 
