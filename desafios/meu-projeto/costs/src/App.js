3bc4c99983df4be601a80bcf69031d54d354c988
Pedro Henrique <92455469+eipedrohenrique@users.noreply.github.com>
2022-05-02 15:41:58 -0300

    projeto

--- desafios/meu-projeto/costs/src/App.js
+++ desafios/meu-projeto/costs/src/App.js
@@ -3,27 +3,26 @@ import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
+      <Navbar/>
       <Container customClass="min-height">
         <Routes> 
           <Route path="/" element={<Home/>}></Route>
+          <Route path="/projects" element={<Projects/>}></Route>
           <Route path="/company" element={<Company/>}></Route>
           <Route path="/contact" element={<Contact/>}></Route>
           <Route path="/newproject" element={<NewProject/>}></Route>
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer/>
     </Router>
   )
 }
