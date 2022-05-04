02d8a02cd6bffd1809dcc8a3f712a326053c3d7e
Pedro Henrique <92455469+eipedrohenrique@users.noreply.github.com>
2022-05-04 17:22:46 -0300

    projeto

--- desafios/meu-projeto/costs/src/components/pages/NewProject.js
+++ desafios/meu-projeto/costs/src/components/pages/NewProject.js
@@ -4,7 +4,7 @@ import styles from './NewProject.module.css'
 
 function NewProject() {
 
-    const history = useNavigate()
+    const navigate = useNavigate()
 
     function createPost(project) {
         project.cost = 0
@@ -20,7 +20,7 @@ function NewProject() {
         .then((resp) => resp.json())
         .then((data) => {
             console.log(data)
-            history('/projects', { message : 'Projeto criado com sucesso!' })
+            navigate('/projects', { state: {message: 'Projeto criado com sucesso!'} })
         })
         .catch(err => console.log(err))
     }
