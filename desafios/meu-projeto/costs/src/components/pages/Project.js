e6e30a4a4d8cd36544a867b486f833fc4b86555d
Pedro Henrique <92455469+eipedrohenrique@users.noreply.github.com>
2022-05-20 13:40:05 -0300

    projeto

--- desafios/meu-projeto/costs/src/components/pages/Project.js
+++ desafios/meu-projeto/costs/src/components/pages/Project.js
@@ -96,7 +96,29 @@ function Project() {
         .catch(err => console.log(err))
     }
 
-    function removeService() {}
+    function removeService(id, cost) {
+        const serviceUpdated = project.services.filter(
+            (service) => service.id !== id
+        )
+
+        const  projectUpdated = project 
+        projectUpdated.services = serviceUpdated
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(projectUpdated)
+        }).then((resp) => resp.json())
+        .then((data) => {
+            setProject(projectUpdated)
+            setServices(serviceUpdated)
+            setMessage('Serviço removido com suecesso!')
+        })
+        .catch(err => console.log(err))
+    }
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
