b2b77708c3fd4a56b6668d7a0ddde4f145d82d37
Pedro Henrique <92455469+eipedrohenrique@users.noreply.github.com>
2022-07-14 19:34:32 -0300

    login

---
+++ login/src/contexts/auth.js
@@ -0,0 +1,11 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState();
+
+    useEffect(()=> {
+        
+    })
+}
